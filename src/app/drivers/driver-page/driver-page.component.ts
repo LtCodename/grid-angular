07bd21c97860ed1c0d7db75dcf0a0197855f911a
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-06-11 00:02:46 -0700

    Dedicated driver page part 1 - ach tab

--- src/app/drivers/driver-page/driver-page.component.ts
+++ src/app/drivers/driver-page/driver-page.component.ts
@@ -14,16 +14,23 @@ export class DriverPageComponent implements OnInit {
   driver: IDriver;
   allDrivers: IDriver[] = [];
   showData: boolean = false;
+  tabName: string = "ach";
+  driverAge: number = 0;
 
   constructor(private route: ActivatedRoute, private driversService: DriversService) {
 
     this.getQueryParam();
     
-    this.driversService.drivers$.subscribe(data => {
+    this.driversService.getDriversData().subscribe(data => {
       this.allDrivers = data;
       this.driver = this.allDrivers.find((dr: IDriver) => dr.id === this.driverId);
       if(this.driver){
+        const dob: number = parseInt(this.driver["date-of-birth"]);
+        const currentYear: number = new Date().getFullYear();
+        this.driverAge = currentYear - dob;
+
         this.showData = true;
+        console.log(this.driver);
       }
     });
   }
@@ -32,6 +39,14 @@ export class DriverPageComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       this.driverId = params.get("driver");
     });
+  }
+
+  toggleTab(): void {
+    if (this.tabName === "ach") {
+      this.tabName = "sum";
+    } else {
+      this.tabName = "ach";
+    }
   }
 
   ngOnInit(): void {
