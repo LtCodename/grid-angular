3926496ec4953be47dd160ea07b593dbf594e533
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-06-29 20:41:58 -0700

    Data Edit Tabs

--- src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, NgZone} from '@angular/core';
 import { DashboardService } from '../dashboard.service';
-import { filter } from 'rxjs/operators';
+import { filter, mergeMap, map } from 'rxjs/operators';
+import { SeasonsService } from 'src/app/seasons/seasons.service';
+import { RacesService } from 'src/app/races/races.service';
+import { DriversService } from 'src/app/drivers/drivers.service';
+import { TeamsService } from 'src/app/teams/teams.service';
+import { ISeason } from 'src/app/seasons/seasons-model';
+import { IRace } from 'src/app/races/race-model';
+import { IDriver } from 'src/app/drivers/drivers-model';
+import { ITeam } from 'src/app/teams/teams-model';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -17,23 +25,83 @@ export class DashboardPageComponent implements OnInit {
   loginButtonText: string = "Login";
   logoutButtonText: string = "Logout";
   editMode: string = "";
+  showData: boolean = false;
 
-  constructor(private dashboardService: DashboardService, private ngZone: NgZone) {
-    this.dashboardService.authToken$
-      .pipe(filter((data) => data !== 'first'))
-      .subscribe(data => {
-        this.ngZone.run(() => {
-          this.user = data;
-          if (this.user) {
-            this.showLoginPanel = false;
-            this.showEditPanel = true;
-          } else {
-            this.showLoginPanel = true;
-            this.showEditPanel = false;
-          }
-          //console.log(this.user);
-        });
+  allSeasons: ISeason[] = [];
+  allRaces: IRace[] = [];
+  allTeams: ITeam[] = [];
+  allDrivers: IDriver[] = [];
+
+  constructor(
+      private dashboardService: DashboardService, 
+      private ngZone: NgZone,
+      private seasonsService: SeasonsService,
+      private racesService: RacesService,
+      private driversService: DriversService,
+      private teamsService: TeamsService,
+    ) {
+    this.dashboardService.authToken$.pipe(filter((data) => data !== 'first')).subscribe(data => {
+      this.ngZone.run(() => {
+        this.user = data;
+        if (this.user) {
+          this.showLoginPanel = false;
+          this.showEditPanel = true;
+        } else {
+          this.showLoginPanel = true;
+          this.showEditPanel = false;
+        }
+        //console.log(this.user);
       });
+    });
+
+    this.seasonsService.seasons$.pipe(
+      mergeMap((allSeasons: ISeason[]) => this.racesService.races$.pipe(map((allRaces: IRace[]): [ISeason[], IRace[]] => [allSeasons, allRaces]))),
+      mergeMap(([allSeasons, allRaces]) => this.driversService.drivers$.pipe(map((allDrivers: IDriver[]): [ISeason[], IRace[], IDriver[]] => [allSeasons, allRaces, allDrivers]))),
+      mergeMap(([allSeasons, allRaces, allDrivers]) => this.teamsService.teams$.pipe(map((allTeams: ITeam[]): [ISeason[], IRace[], IDriver[], ITeam[]] => [allSeasons, allRaces, allDrivers, allTeams])))
+    ).subscribe(([allSeasons, allRaces, allDrivers, allTeams]) => {
+      if (allSeasons.length && allRaces.length && allDrivers.length && allTeams.length) {
+        this.allSeasons = allSeasons.sort((a: ISeason, b: ISeason) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+        this.allDrivers = allDrivers.sort((a: IDriver, b: IDriver) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+        this.allTeams = allTeams.sort((a: ITeam, b: ITeam) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+        this.allRaces = allRaces.sort((a: IRace, b: IRace) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+
+      this.showData = true;
+    }, () => {
+      this.showData = true;
+    });
   }
 
   ngOnInit(): void {
--- src/app/drivers/drivers-page/drivers-page.component.ts
+++ src/app/drivers/drivers-page/drivers-page.component.ts
@@ -29,7 +29,7 @@ export class DriversPageComponent implements OnInit {
           return 1;
         }
         return 0;
-      })
+      });
 
       if(this.drivers.length) {
         this.showPreloader = false;
